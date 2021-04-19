9c69585d9fe8f1ad511e64c6d007a2aab1185d56
Haz Hamed <hhamed@MacBook-Pro-3.local>
2021-04-19 13:31:43 -0700

    fixed the html, added new logs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JokeBot
-
+1- run index.html on 5501 with ngrok 
+2 run webhooks.js on 5501 with nodemon
+3- make sure that the app https://dev.drift.com/app/bd0ff105-74a1-4b59-b00d-10c05ca29bee/events 
+    add ngrok url to
+        oauth 
+        webhooks tabs
--- webhooks.js
+++ webhooks.js
@@ -1,73 +1,68 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const Drift = require('drift-chat');
-var request2 = require('request');
-const { request } = require('express');
-
-
+const bodyParser = require("body-parser");
+const Drift = require("drift-chat");
+var request2 = require("request");
+const { request } = require("express");
 
 app.use(bodyParser.json());
-app.listen(process.env.PORT || 5500, () => console.log(`Your bot is listening on port 5500!`));
+app.listen(process.env.PORT || 5501, () =>
+  console.log(`Your bot is listening on port 5501!`)
+);
 
 //shows that your public domain is hooked up
-app.get('/', async(request, response) => {
-
-    // send a response to the html page that is loading on a port
- return response.status(200).send('check it out!!')
-})
+app.get("/", async (request, response) => {
+  // send a response to the html page that is loading on a port
+  return response.status(200).send("check it out!!");
+});
 
 //printing the message body in terminal when you receive the event
-app.post('/', async(request, response) => {
+app.post("/", async (request, response) => {
+
+  // console.log("I am getting something before post")
+
+  // log the whole response body
+  //  console.log(request.body);
+
+  //chat type
+  console.log("Chat type is --->> " + request.body.type);
+
+  // conversationId
+  // console.log("conversationId  --->> " +request.body.data.conversationId);
+
+  // Chat body
+  console.log("Chat body is --->> " + request.body.data.body);
+
+  // orgId
+  console.log("Org ID --->> " + request.body.orgId);
+
+//  author type 
+console.log("Author info --->> " + JSON.stringify(request.body.data.author) );
 
 
-// log the whole response body
-//  console.log(request.body);
-
-console.log("Chat type is --->> " +request.body.type);
-
-// conversationId
-console.log("conversationId  --->> " +request.body.data.conversationId);
-
-
-console.log("Chat body is --->> " +request.body.data.body);
-
-
-
-
-
-if (request.method == "POST") {
-
-//terminal will print a uncolored json response
-// console.log(JSON.stringify(request.body))
-
-
-//terminal will print a colored json response
-// console.log(request.body)
-
-
-
-// Send a response back to the chat as a new chat
-// var options = {
-//     'method': 'POST',
-//     'url': 'https://driftapi.com/conversations/3098989957/messages',
-//     'headers': {
-//       'Authorization': 'Bearer 9pBAHSXwzcibygiemAmitv0GV59qc7Cx',
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       "body": "response from postman",
-//       "type": "chat"
-//     })
-//   };
-
-//   request2(options, function (error, response) {
-//     if (error) throw new Error(error);
-//     console.log(response.body);
-//   });
-
-    }
-
-
-})
+  if (request.method == "POST") {
+    //log something
+    // console.log("I am getting something after post")
+    //terminal will print a uncolored json response
+    // console.log(JSON.stringify(request.body))
+    //terminal will print a colored json response
+    // console.log(request.body)
+    // Send a response back to the chat as a new chat
+    // var options = {
+    //     'method': 'POST',
+    //     'url': 'https://driftapi.com/conversations/3098989957/messages',
+    //     'headers': {
+    //       'Authorization': 'Bearer 9pBAHSXwzcibygiemAmitv0GV59qc7Cx',
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //       "body": "response from postman",
+    //       "type": "chat"
+    //     })
+    //   };
+    //   request2(options, function (error, response) {
+    //     if (error) throw new Error(error);
+    //     console.log(response.body);
+    //   });
+  }
+});
